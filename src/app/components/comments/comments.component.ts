0844a14d253313219ae3d71f24c871d765266bef
ivanys23071996 <ivanys230796@gmail.com>
2022-05-12 10:45:38 +0300

    comments

---
+++ src/app/components/comments/comments.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {CommentsService} from "../../services/comments.service";
+import {IComment} from "../../models/IComment";
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.css']
+})
+export class CommentsComponent implements OnInit {
+
+  comments: IComment[];
+
+  constructor(private commentsService : CommentsService) { }
+
+  ngOnInit(): void {
+    this.commentsService.getComments().subscribe(value => this.comments = value)
+  }
+
+}
