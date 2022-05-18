867abeca625811b324a81fba625dfde0df314527
ivanys23071996 <ivanys230796@gmail.com>
2022-05-18 22:11:25 +0300

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {UsersComponent} from "./components/users/users.component";
-import {UserComponent} from "./components/user/user.component";
-import {PostsComponent} from "./components/posts/posts.component";
-import {PostComponent} from "./components/post/post.component";
-import {CommentsComponent} from "./components/comments/comments.component";
-import {CommentComponent} from "./components/comment/comment.component";
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {AppRoutingModule} from "./app.routing.module";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    PostComponent,
-    CommentsComponent,
-    CommentComponent
+    HomeComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
