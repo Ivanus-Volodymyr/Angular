3c619178bf3d1d53f4c81f504e1941f841feb40b
ivanys23071996 <ivanys230796@gmail.com>
2022-05-19 16:25:20 +0300

    post module

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClientModule} from "@angular/common/http";
 
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.components/posts/posts.component';
+import {PostsService} from "./posts.service";
+import { PostComponent } from './posts.components/post/post.component';
+import { PostDetailsComponent} from './posts.components/post.details/post.details.component';
 
 
 @NgModule({
   declarations: [
-    PostsComponent
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent
   ],
   imports: [
     CommonModule,
-    PostsRoutingModule
-  ]
+    PostsRoutingModule,
+    HttpClientModule,
+  ],
+  providers:[PostsService]
 })
 export class PostsModule { }
