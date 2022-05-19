3d81c0afd8056b0b5f69fa69536df6c305481c47
ivanys23071996 <ivanys230796@gmail.com>
2022-05-19 16:22:36 +0300

    posts services

---
+++ src/app/modules/posts/posts.service/posts.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {urls} from "../../../constants";
+import {IPost} from "../posts.models";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getPosts(): Observable<IPost[]> {
+    return this.httpClient.get<IPost[]>(urls.posts);
+  }
+  getPostById(id: string): Observable<IPost>{
+    return this.httpClient.get<IPost>(`${urls.posts}/${id}`)
+  }
+}
