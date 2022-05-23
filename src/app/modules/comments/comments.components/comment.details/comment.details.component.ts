0b358a521a2c5dbeebe0faab43a75f1c7940a4e1
ivanys23071996 <ivanys230796@gmail.com>
2022-05-23 19:33:56 +0300

    comments details resolver

--- src/app/modules/comments/comments.components/comment.details/comment.details.component.ts
+++ src/app/modules/comments/comments.components/comment.details/comment.details.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
-import {CommentsService} from "../../comments.service";
 import {IComment} from "../../comments.models";
 
 @Component({
@@ -12,18 +11,12 @@ import {IComment} from "../../comments.models";
 export class CommentDetailsComponent implements OnInit {
 
   commentDetail: IComment;
-  commentId: string;
 
-  constructor(private commentsService: CommentsService, private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    if (history.state.data) {
-      this.activatedRoute.params.subscribe(value => this.commentDetail = history.state.data);
-      this.activatedRoute.params.subscribe(value => this.commentId = value['id']);
-    }
-    this.activatedRoute.params.subscribe(value => this.commentId = value['id']);
-    this.commentsService.getCommentById(this.commentId).subscribe(value => this.commentDetail = value);
+    this.activatedRoute.data.subscribe(({commentDetailsData}) => this.commentDetail = commentDetailsData)
   }
 
 }
---
+++ src/app/modules/comments/comments.service/comment.details.resolver.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {
+  Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import {Observable} from 'rxjs';
+
+import {IComment} from "../comments.models";
+import {CommentsService} from "./comments.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentDetailsResolver implements Resolve<IComment> {
+
+  constructor(private commentsService: CommentsService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IComment> | Promise<IComment> | IComment {
+    const id = route.paramMap.get('id')
+    return this.commentsService.getCommentById(id);
+  }
+
+}
