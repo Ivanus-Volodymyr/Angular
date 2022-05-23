ba0faf3ab5dafabe183770fc709a7692930017ae
ivanys23071996 <ivanys230796@gmail.com>
2022-05-23 22:13:35 +0300

    post resolver

--- src/app/modules/posts/posts.service/post.details.resolver.ts
+++ src/app/modules/posts/posts.service/post.details.resolver.ts
@@ -2,9 +2,10 @@ import {Injectable} from '@angular/core';
 import {
   Resolve,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot, Router
 } from '@angular/router';
 import {Observable} from 'rxjs';
+
 import {IPost} from "../posts.models";
 import {PostsService} from "./posts.service";
 
@@ -13,10 +14,14 @@ import {PostsService} from "./posts.service";
 })
 export class PostDetailsResolver implements Resolve<IPost> {
 
-  constructor(private postsService: PostsService) {
+  constructor(private postsService: PostsService, private router: Router) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IPost> | Promise<IPost> | IPost {
+    const post = this.router.getCurrentNavigation()?.extras?.state?.['post'] as IPost;
+    if (post) {
+      return post;
+    }
     const id = route.paramMap.get('id');
     return this.postsService.getPostById(id);
   }
