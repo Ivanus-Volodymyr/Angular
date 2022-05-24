0d5d925a2ccbcb496d4b41b3185a4102da553306
ivanys23071996 <ivanys230796@gmail.com>
2022-05-24 15:44:44 +0300

    posts guard

--- src/app/modules/posts/posts-routing.module.ts
+++ src/app/modules/posts/posts-routing.module.ts
@@ -4,11 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {PostsComponent} from "./posts.components/posts/posts.component";
 import {PostDetailsComponent} from "./posts.components/post.details/post.details.component";
 import {PostDetailsResolver, PostsResolver} from "./posts.service";
+import {PostsGuard} from "./guards";
 
 const routes: Routes = [
   {
     path: '', component: PostsComponent,
     resolve: {postsData: PostsResolver},
+    canActivate: [PostsGuard],
     children: [
       {
         path: 'details/:id', component: PostDetailsComponent,
--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -1,25 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
 
-import { PostsRoutingModule } from './posts-routing.module';
-import { PostsComponent } from './posts.components/posts/posts.component';
+import {PostsRoutingModule} from './posts-routing.module';
+import {PostsComponent} from './posts.components/posts/posts.component';
 import {PostDetailsResolver, PostsResolver, PostsService} from "./posts.service";
-import { PostComponent } from './posts.components/post/post.component';
-import { PostDetailsComponent} from './posts.components/post.details/post.details.component';
+import {PostComponent} from './posts.components/post/post.component';
+import {PostDetailsComponent} from './posts.components/post.details/post.details.component';
+import {PostsGuard} from "./guards";
 
 
 @NgModule({
   declarations: [
     PostsComponent,
     PostComponent,
-    PostDetailsComponent
+    PostDetailsComponent,
   ],
   imports: [
     CommonModule,
     PostsRoutingModule,
     HttpClientModule,
   ],
-  providers:[PostsService, PostsResolver, PostDetailsResolver]
+  providers: [PostsService, PostsResolver, PostDetailsResolver, PostsGuard],
 })
-export class PostsModule { }
+export class PostsModule {
+}
