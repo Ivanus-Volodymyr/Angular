ca53e7eceb830412190b47ce58fe21686e1b8770
ivanys23071996 <ivanys230796@gmail.com>
2022-05-25 18:36:13 +0300

    auth module

--- src/app/modules/auth/auth.components/login/login.component.ts
+++ src/app/modules/auth/auth.components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+
+import {AuthService} from "../../auth.services";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form_login: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this._createForm()
+  }
 
   ngOnInit(): void {
   }
 
+  _createForm(): void {
+    this.form_login = new FormGroup({
+      username: new FormControl(null),
+      password: new FormControl(null),
+    });
+  }
+
+  login(): void {
+    this.authService.login(this.form_login.getRawValue()).subscribe(value => {
+      this.authService.setToken(value);
+      this.router.navigate(['cars']);
+    })
+  }
 }
--- src/app/modules/auth/auth.services/auth.service.ts
+++ src/app/modules/auth/auth.services/auth.service.ts
@@ -2,18 +2,39 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-import {IUser} from "../auth.models";
+import {IToken, IUser} from "../auth.models";
 import {urls} from "../../../constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private access = 'access';
 
   constructor(private httpClient: HttpClient) {
   }
 
   registration(user: IUser): Observable<IUser> {
     return this.httpClient.post<IUser>(urls.users, user)
+  }
+
+  login(user: IUser): Observable<IToken> {
+    return this.httpClient.post<IToken>(urls.auth, user)
+  }
+
+  setToken(token: IToken): void {
+    localStorage.setItem(this.access, token.access)
+  }
+
+  getToken(): string {
+    return localStorage.getItem(this.access)
+  }
+
+  deleteToken(): void {
+    localStorage.removeItem(this.access)
+  }
+
+  isAuthorization(): boolean {
+    return !!localStorage.getItem(this.access)
   }
 }
