f72d148da25d6fc1550700d3aad3d8238869f70d
ivanys23071996 <ivanys230796@gmail.com>
2022-05-25 18:37:03 +0300

    main interceptor

---
+++ src/app/main.interceptor.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import {catchError, Observable} from 'rxjs';
+import {Router} from "@angular/router";
+
+import {AuthService} from "./modules/auth/auth.services";
+
+
+@Injectable()
+export class MainInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const authorized = this.authService.isAuthorization();
+
+    if (authorized) {
+      const token = this.authService.getToken();
+      request = this.addToken(request, token)
+    }
+    return next.handle(request).pipe(
+      // @ts-ignore
+      catchError((response: HttpErrorResponse) => {
+        if (response && response.error && response.status === 401) {
+          this.authService.deleteToken();
+          this.router.navigate(['login'])
+        }
+      }, )
+    );
+  }
+
+  addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {Authorization: `Bearer ${token}`}
+    })
+  }
+}
