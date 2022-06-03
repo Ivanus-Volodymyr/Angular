48a5fe773bc4b902e3d9f0f5cc48208865929c06
ivanys23071996 <ivanys230796@gmail.com>
2022-06-03 22:53:57 +0300

    project

--- src/app/modules/auth/components/registration/registration.component.ts
+++ src/app/modules/auth/components/registration/registration.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+
+  form: FormGroup;
+  userNameError: string;
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    this._createForm()
+  }
+
+  _createForm(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null, [Validators.minLength(2), Validators.maxLength(15), Validators.required]),
+      password: new FormControl(null, [Validators.minLength(2), Validators.maxLength(15), Validators.required]),
+      confirmPassword: new FormControl(null, [Validators.minLength(2), Validators.maxLength(20), Validators.required]),
+    }, [this._checkPasswords]);
+  }
+
+  register(): void {
+    const formData = this.form.getRawValue();
+    delete formData.confirmPassword;
+    this.router.navigate([''])
+  }
+
+  _checkPasswords(form: AbstractControl): ValidationErrors | null {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    return password.value === confirmPassword.value ? null : {notSame: 'Password not confirm'};
   }
 
 }
